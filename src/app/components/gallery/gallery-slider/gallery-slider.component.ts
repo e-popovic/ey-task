9819b7cb368e248c9e580bb033592aa0b59bf26d
e-popovic <popovic.ema59@gmail.com>
2023-10-15 11:45:30 +0200

    gallery carousel

--- src/app/components/gallery/gallery-slider/gallery-slider.component.ts
+++ src/app/components/gallery/gallery-slider/gallery-slider.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { interval, Observable, startWith, Subject, switchMap, timer, } from 'rxjs';
+import {MatDialog, MatDialogModule, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { ImageDialog } from '../gallery-grid/gallery-grid.component'
 
 @Component({
   selector: 'app-gallery-slider',
@@ -7,7 +10,9 @@ import { Component } from '@angular/core';
 })
 export class GallerySliderComponent {
 
-  imageUrls = [
+  constructor(public dialog: MatDialog) { }
+
+  slides = [
     '../../../../assets/Rectangle.png',
     '../../../../assets/RectangleCopy2.png',
     '../../../../assets/RectangleCopy3.png',
@@ -15,6 +20,59 @@ export class GallerySliderComponent {
     '../../../../assets/img_2.png',
     // Add more image URLs as needed
   ];
-  currentIndex = 0;
+
+  currentIndex: number = 0;
+  timeoutId?: number;
+
+  ngOnInit(): void {
+    this.resetTimer();
+  }
+  ngOnDestroy() {
+    window.clearTimeout(this.timeoutId);
+  }
+  resetTimer() {
+    if (this.timeoutId) {
+      window.clearTimeout(this.timeoutId);
+    }
+    this.timeoutId = window.setTimeout(() => this.goToNext(), 3000);
+  }
+
+  goToPrevious(): void {
+    const isFirstSlide = this.currentIndex === 0;
+    const newIndex = isFirstSlide
+      ? this.slides.length - 1
+      : this.currentIndex - 1;
+
+    this.resetTimer();
+    this.currentIndex = newIndex;
+  }
+
+  goToNext(): void {
+    const isLastSlide = this.currentIndex === this.slides.length - 1;
+    const newIndex = isLastSlide ? 0 : this.currentIndex + 1;
+
+    this.resetTimer();
+    this.currentIndex = newIndex;
+  }
+
+  goToSlide(slideIndex: number): void {
+    this.resetTimer();
+    this.currentIndex = slideIndex;
+  }
+
+  getCurrentSlideUrl() {
+    return `url('${this.slides[this.currentIndex]}')`;
+  }
+
+  getCurrentSlideName() {
+    let splitter = this.getCurrentSlideUrl().split('/');
+    return splitter[splitter.length - 1].slice(0, -2);
+  }
+
+  openDialog() {
+    this.dialog.open(ImageDialog, {
+      data: this.getCurrentSlideName(),
+    });
+  }
 
 }
